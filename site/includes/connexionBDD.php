93272f7dc7a6c212e57b647fcaa487cbd20084b5
Loic HARRANG <loic.harrang@etudiant.univ-rennes1.fr>
2023-10-16 10:37:51 +0200

    résolution bug site -> BDD (ajout d'un nom au container des bdd)

--- site/includes/connexionBDD.php
+++ site/includes/connexionBDD.php
@@ -1,7 +1,7 @@
 <?php
     try{
         // Connexion à la bdd
-        $BDD = new PDO('pgsql:host=psql;port=5432;dbname=site;', 'loic', '123456789');
+        $BDD = new PDO('pgsql:host=psql;port=5432;dbname=site', 'loic', '123456789');
         // Pour éviter de faire PDO::FETCH_ASSOC à chaque fois qu'il faut récupérer des résultats
         $BDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         // Récupère les erreurs dans la variable BDD (plus simple à débug)
